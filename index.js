1c9a79a0b4e48350d23275feb84247108e141f45
MauroKkkk <mmcaf1@aluno.ifal.edu.br>
2022-11-09 18:02:52 -0300

    1.0.1

--- index.js
+++ index.js
@@ -15,24 +15,44 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/api/login', (req, res) => {
-    const password = req.body.password
+app.post('/login', (req, res) => {
+
     const email = req.body.email
+    const password = req.body.password
     
-    const sqlSelect = "SELECT (email, password) FROM Client WHERE email = 'cuzinho@gmail.com' and password = 123123 "
-    db.query(sqlSelect, [password, email], (err, result) => {
-        console.log(result)
+    const sqlSelect = "SELECT email, password FROM Client WHERE email = ? and password = ?"
+    db.query(sqlSelect, [email, password], (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            if(result){
+                console.log(result)
+            }else{
+                console.log("Senha ou email errados")
+            }
+        }
     })
 } )
 
-app.post('/api/insert', (req, res) => {
+app.post('/cadastro', (req, res) => {
 
-    const password = req.body.password
     const email = req.body.email
+    const password = req.body.password
     
-    const sqlInsert = 'INSERT INTO Client (name, email) VALUES (?,?)'
-    db.query(sqlInsert, [password, email], (err, result) => {
-         console.log(result)
+    const sqlInsert = 'INSERT INTO Client (email, password) VALUES (?,?)'
+    db.query(sqlInsert, [email, password], (err, result) => {
+        if(err){
+            res.send(err)
+            console.log(1)
+        }else{
+            if(result){
+                console.log(result)
+                console.log(2)
+            }else{
+                console.log("Senha ou email errados")
+                console.log(3)
+            }
+        }
     })
 })
 app.listen(3001, () => {
